e482a784544cce91c883c67a4a3dbfc8ee244c43
Avi Mualem <Avi.Mualem@sharegain.com>
2019-01-27 16:11:31 +0200

    no message

--- DataLayer/DocumentDb/ConnectionManager.py
+++ DataLayer/DocumentDb/ConnectionManager.py
@@ -5,14 +5,13 @@ import  os
 
 _Session = None
 #
-# config = configparser.ConfigParser()
-# configFile = config.read('Config.ini')
-# documentDbConnectionDetails.documentdkey = config['DocumentDbConnectionDetails']['key']
-# documentDbConnectionDetails.documentdburi = config['DocumentDbConnectionDetails']['uri']
-# documentDbConnectionDetails.collectionname = config['DocumentDbConnectionDetails']['CollectionName']
-# documentDbConnectionDetails.dbname = config['DocumentDbConnectionDetails']['DbName']
-_Session = document_client.DocumentClient \
-        (os.environ['db_uri'], {'masterKey': os.environ['db_key']})
+config = configparser.ConfigParser()
+configFile = config.read('Config.ini')
+documentDbConnectionDetails.documentdkey = config['DocumentDbConnectionDetails']['key']
+documentDbConnectionDetails.documentdburi = config['DocumentDbConnectionDetails']['uri']
+documentDbConnectionDetails.collectionname = config['DocumentDbConnectionDetails']['CollectionName']
+documentDbConnectionDetails.dbname = config['DocumentDbConnectionDetails']['DbName']
+_Session = document_client.DocumentClient(documentDbConnectionDetails.documentdburi, {'masterKey': documentDbConnectionDetails.documentdkey})
 
 
 def get_session():
